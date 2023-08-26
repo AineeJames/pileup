0df83f04b6c5aef177b0efbfb96479b764384003
Aiden Olsen <aineejames@Aidens-iMac.local>
2023-08-26 13:16:52 -0600

    getting somewhere

--- src/pileup.c
+++ src/pileup.c
@@ -1,4 +1,24 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+#define STACK_CAPACITY 4000
+
+typedef enum { PUSH_INT, PLUS, PRINT } TokenType;
+
+typedef union {
+  int64_t i;
+} TokenValue;
+
+typedef struct {
+  TokenType type;
+  TokenValue value;
+  int line_number;
+} Token;
+
+typedef struct {
+  int64_t stack[STACK_CAPACITY];
+} PileupState;
 
 int main(int argc, char *argv[]) {
   const char *code = "10 10 + print";
